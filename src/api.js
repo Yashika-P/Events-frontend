248204f9ff326fd4c466c045f82f742a32ff58b1
Yashika-P <yyashika118@gmail.com>
2024-12-24 18:49:36 +0530

    Added Commit

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://events-task-backend-p52x.onrender.com/api" });
 
 export const registerUser = (data) => API.post("/users/register", data);
 export const loginUser = (data) => API.post("/users/login", data);
